4287a0f99e3aeb08831289812eefca2b28e33908
Vinicius <vinicius_uo@hotmail.com>
2025-05-16 14:56:03 -0300

    feat: Add route and DBconnection to server file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,24 @@
 import express from "express";
+import orderRoutes from "./routes/order.routes";
+import mongoConnect from "./configs/database";
+import { config } from "./configs";
 
 const app = express();
+
 app.use(express.json());
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.use("/api/sales", orderRoutes);
+
+mongoConnect()
+  .then(() => {
+    app.listen(config.port, () =>
+      console.log(`Server running on port: ${config.port}`),
+    );
+  })
+  .catch((error: unknown) => {
+    console.error('MongoDB connection failed:\n', error);
+    console.log('Server initialization cancelled');
+    process.exit(1);
+  });
+
+
